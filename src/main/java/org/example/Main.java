093eda603547d2a326e730551050132cd99b023c
Michael Evans <Michael.evans1775@outlook.com>
2022-10-20 16:34:48 -0600

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,90 @@
+package org.example;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args)throws Exception{
+        callWeather weather = new callWeather();
+
+        weather.getData();
+
+    }
+
+    static class callWeather{
+
+        public void getData() throws Exception {
+            String username = "student_evans";
+            String password = "0xAD5bdm6R";
+            System.out.print("username: " + username + ", password: " + password + "\n");
+            URL url = new URL("https://api.meteomatics.com/2022-10-20T00:00:00Z/t_2m:F/40.479961,-104.907677/json?model=mix");
+            String encoding = Base64.getEncoder().encodeToString((username + ":" + password).getBytes(StandardCharsets.UTF_8));
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestProperty("Authorization", "Basic " + encoding);
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+            }
+
+            BufferedReader streamReader = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            StringBuilder responseStrBuilder = new StringBuilder();
+
+            String inputStr;
+            while ((inputStr = streamReader.readLine()) != null) {
+                responseStrBuilder.append(inputStr);
+            }
+
+            System.out.print(responseStrBuilder);
+
+
+            Path path = Paths.get("/Users/michael.evans/IdeaProjects/Weather/src/main/java/org/example/data.json");
+            try {
+                Files.writeString(path, responseStrBuilder.toString(), StandardCharsets.UTF_8);
+            } catch (IOException ex) {
+                System.out.println("invalid path");
+            }
+
+//          JSON parser with object
+            JSONParser parser = new JSONParser();
+            Reader reader = new FileReader("/Users/michael.evans/IdeaProjects/Weather/src/main/java/org/example/data.json");
+            Object object = parser.parse(reader);
+            JSONObject jsonObject = (JSONObject) object;
+
+            Integer value = (Integer) jsonObject.get("value");
+            System.out.println("The value im looking for" + " " + value);
+
+
+            //json parser with array.
+
+            JSONObject jsonObject1 = (JSONObject) object;
+            JSONArray array1 = (JSONArray) jsonObject1.get("data");
+            Iterator i = array1.iterator();
+
+            while(i.hasNext()){
+                JSONObject slide = (JSONObject) i.next();
+                Double target = (Double) slide.get("value");
+                System.out.println("Value given arrays" + " " + target + " " + array1);
+
+            }
+
+        }
+        }
+
+    }
