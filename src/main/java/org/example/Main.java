6528dc6f721f97251dff012cc5d5c3d634050a5f
Michael Evans <Michael.evans1775@outlook.com>
2022-10-27 16:06:41 -0600

    wip added notes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,8 @@ public class Main {
         System.out.println("data incoming");
         parser.read();
 
-
+        //witihin the parser class try to implement a variable for each position on the json file.
+        // the issue may be that there are not enough variables to properly collected data.
 
 
     }
--- src/main/java/org/example/ParseJson.java
+++ src/main/java/org/example/ParseJson.java
@@ -10,8 +10,6 @@ public class ParseJson {
     public void read() throws Exception{
         try {
             Reader reader = new FileReader("/Users/michael.evans/IdeaProjects/Weather/src/main/java/org/example/data.json");
-            System.out.println("reader" + reader.toString());
-
             Gson gson = new Gson();
             getData data = gson.fromJson(reader.toString(), getData.class);
             System.out.println(data.getParam());
